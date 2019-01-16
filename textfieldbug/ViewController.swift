fb745d51358ad1fa50ac50a73948b724fd9cc22c
Denis Batvinkin <d.batvinkin@rambler-co.ru>
2019-01-17 00:08:56 +0300

    Initial Commit

---
+++ textfieldbug/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  textfieldbug
+//
+//  Created by Denis Batvinkin on 17/01/2019.
+//  Copyright © 2019 func. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
